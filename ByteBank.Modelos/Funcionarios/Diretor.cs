dd500ad46fe0946abac10593392fda232fa3f6d4
Rodolfo Alves <rodolfo.s.a@hotmail.com>
2021-06-10 18:21:27 -0300

    Commit inicial do projeto ja existente

---
+++ ByteBank.Modelos/Funcionarios/Diretor.cs
@@ -0,0 +1,30 @@
+﻿using ByteBank.Modelos.Funcionarios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.Modelos.Funcionarios
+{
+    public class Diretor : FuncionarioAutenticavel
+    {
+        //public string Nome { get; set; }
+        //public string CPF { get; set; }
+        //public double Salario { get; set; }
+        public Diretor(string cpf) : base(5000, cpf)
+        {
+
+        }
+
+        public override double GetBonficacao()
+        {
+            return Salario *= 0.5;
+        }
+
+        public override void AumentarSalario()
+        {
+            Salario *= 1.15;
+        }
+    }
+}
