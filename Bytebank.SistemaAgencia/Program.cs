d5264ea40002a24302f767b246a0cdfc46fd6321
Rodolfo Alves <rodolfo.s.a@hotmail.com>
2021-06-11 21:29:50 -0300

    Alterações para inicar a aula4 da parte 7 do curso de C#

--- Bytebank.SistemaAgencia/Program.cs
+++ Bytebank.SistemaAgencia/Program.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using ByteBank.Modelos;
 using ByteBank.Modelos.Funcionarios;
@@ -13,138 +12,80 @@ namespace ByteBank.SistemaAgencia
     {
         static void Main(string[] args)
         {
-            // Olá, meu nome é Guilherme e você pode entrar em contato comigo
-            // através do número 8457-4456!
+            //ListaDeContaCorrente lista = new ListaDeContaCorrente();
+            ListaDeContaCorrente lista = new ListaDeContaCorrente();
 
-            // Meu nome é Guilherme, me ligue em 4784-4546
+            ContaCorrente contaDoGui = new ContaCorrente(11111, 1111111);
+            lista.Adicionar(contaDoGui);
+            lista.Adicionar(new ContaCorrente(874, 5679787));
+            lista.Adicionar(new ContaCorrente(874, 5679754));
+            lista.Adicionar(new ContaCorrente(874, 5679445));
+            lista.Adicionar(new ContaCorrente(874, 5679445));
+            lista.Adicionar(new ContaCorrente(874, 5679445));
+            lista.Adicionar(new ContaCorrente(874, 5679445));
+            lista.Adicionar(new ContaCorrente(874, 5679445));
+            lista.Adicionar(new ContaCorrente(874, 5679445));
+            lista.Adicionar(new ContaCorrente(874, 5679445));
+            lista.Adicionar(new ContaCorrente(874, 5679445));
+            lista.Adicionar(new ContaCorrente(874, 5679445));
 
-            //  "[0123456789][0123456789][0123456789][0123456789][-][0123456789][0123456789][0123456789][0123456789]";
-            //  "[0-9][0-9][0-9][0-9][-][0-9][0-9][0-9][0-9]";
-            //  "[0-9]{4,5}[-][0-9]{4}";
-            //  "[0-9]{4,5}[-]{0,1}[0-9]{4}";
-            //  "[0-9]{4,5}-{0,1}[0-9]{4}";
-            string padrao = "[0-9]{4,5}-?[0-9]{4}";
+            lista.EscreverListaNaTela();
 
-            // 879.546.120-20
-            // 879546120-20
+            lista.Remover(contaDoGui);
 
-            string textoDeTeste = "idyufdgfugfjksdhf 99871--5456 sdjkfgsdjgsjgh sfsdjgsdjghsdfj";
+            Console.WriteLine("Após remover o item");
 
-            Match resultado = Regex.Match(textoDeTeste, padrao);
+            lista.EscreverListaNaTela();
 
-            Console.WriteLine(resultado.Value);
-            Console.ReadLine();
-
-
-
-
-
-
-
-
-            string urlTeste = "https://www.bytebank.com/cambio";
-            int indiceByteBank = urlTeste.IndexOf("https://www.bytebank.com");
-
-
-
-
-            Console.WriteLine(urlTeste.StartsWith("https://www.bytebank.com"));
-            Console.WriteLine(urlTeste.EndsWith("cambio/"));
-
-
-            Console.WriteLine(urlTeste.Contains("ByteBank"));
-
-            
-            Console.ReadLine();
-
-            // pagina?argumentos
-            // 012345678
-
-
-
-            //moedaOrigem=real&moedaDestino=dolar
-            //          |
-            //  -------´
-
-            string urlParametros = "http://www.bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar&valor=1500";
-            ExtratorValorDeArgumentosURL extratorDeValores = new ExtratorValorDeArgumentosURL(urlParametros);
-
-            string valor = extratorDeValores.GetValor("moedaDestino");
-            Console.WriteLine("Valor de moedaDestino: " + valor);
-
-            string valorMoedaOrigem = extratorDeValores.GetValor("moedaOrigem");
-            Console.WriteLine("Valor de moedaOrigem: " + valorMoedaOrigem);
-
-            Console.WriteLine(extratorDeValores.GetValor("VALOR"));
 
             Console.ReadLine();
-
-
-
-            //Testando ToLower
-            string mensagemOrigem = "PALAVRA";
-            string termoBusca = "ra";
-
-            Console.WriteLine(mensagemOrigem.ToLower());
-
-
-            // Testando Replace
-            termoBusca = termoBusca.Replace('r', 'R');
-            Console.WriteLine(termoBusca);
-
-            termoBusca = termoBusca.Replace('a', 'A');
-            Console.WriteLine(termoBusca);
-
-            Console.WriteLine(mensagemOrigem.IndexOf(termoBusca));
-            Console.ReadLine();
-
-            // Testando o método Remove
-            string testeRemocao = "primeiraParte&123456789";
-            int indiceEComercial = testeRemocao.IndexOf('&');
-            Console.WriteLine(testeRemocao.Remove(indiceEComercial, 4));
-            Console.ReadLine();
-
-
-            // <nome>=<valor>
-            string palavra = "moedaOrigem=moedaDestino&moedaDestino=dolar";
-            string nomeArgumento = "moedaDestino=";
-
-            int indice = palavra.IndexOf(nomeArgumento);
-            Console.WriteLine(indice);
-
-            Console.WriteLine("Tamanho da string nomeArgumento: " + nomeArgumento.Length);
-
-            Console.WriteLine(palavra);
-            Console.WriteLine(palavra.Substring(indice));
-            Console.WriteLine(palavra.Substring(indice + nomeArgumento.Length));
-            Console.ReadLine();
-
-
-
-
-
-            // Testando o IsNullOrEmpty
-            string textoVazio = "";
-            string textoNulo = null;
-            string textoQualquer = "kjhfsdjhgsdfjksdf";
-            Console.WriteLine(String.IsNullOrEmpty(textoVazio));
-            Console.WriteLine(String.IsNullOrEmpty(textoNulo));
-            Console.WriteLine(String.IsNullOrEmpty(textoQualquer));
-            Console.ReadLine();
-
-            ExtratorValorDeArgumentosURL extrator = new ExtratorValorDeArgumentosURL("");
-
-            string url = "pagina?moedaOrigem=real&moedaDestino=dolar";
-
-            int indiceInterrogacao = url.IndexOf('?');
-
-            Console.WriteLine(indiceInterrogacao);
-
-            Console.WriteLine(url);
-            string argumentos = url.Substring(indiceInterrogacao + 1);
-            Console.WriteLine(argumentos);
         }
+
+        static void TestaArrayDeContaCorrente()
+        {
+            ContaCorrente[] contas = new ContaCorrente[]
+                {
+                    new ContaCorrente(874, 5679787),
+                    new ContaCorrente(874, 4456668),
+                    new ContaCorrente(874, 7781438)
+                };
+
+            for (int indice = 0; indice < contas.Length; indice++)
+            {
+                ContaCorrente contaAtual = contas[indice];
+                Console.WriteLine($"Conta {indice} {contaAtual.Numero}");
+            }
+        }
+
+        static void TestaArrayInt()
+        {
+            // ARRAY de inteiros, com 5 posições!
+            int[] idades = null;
+            idades = new int[3];
+
+            idades[0] = 15;
+            idades[1] = 28;
+            idades[2] = 35;
+            //idades[3] = 50;
+            //idades[4] = 28;
+            //idades[5] = 60;
+
+            Console.WriteLine(idades.Length);
+
+            int acumulador = 0;
+            for (int indice = 0; indice < idades.Length; indice++)
+            {
+                int idade = idades[indice];
+
+                Console.WriteLine($"Acessando o array idades no índice {indice}");
+                Console.WriteLine($"Valor de idades[{indice}] = {idade}");
+
+                acumulador += idade;
+            }
+
+            int media = acumulador / idades.Length;
+            Console.WriteLine($"Média de idades = {media}");
+        }
+
     }
-
-
 }
